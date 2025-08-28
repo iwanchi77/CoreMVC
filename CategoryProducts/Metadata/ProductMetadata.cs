0187ae65be529c801327815a391ab658761b01df
chi <iwanchi1023@gamil.com>
2025-08-28 12:04:52 +0800

    8/28上午課程
    Index(新增商品的超連結改中文、加入icon、改成btn)、美化table的樣式、Lab1加入網站範本(StartBootstrap.com)、建立Patial(Product)、建立Metadata資料夾(放ProductMetadata)、製作Metadata類別(實作資料顯示格式、資料驗證、中文欄位名稱)

---
+++ CategoryProducts/Metadata/ProductMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CategoryProducts.Metadata
+{
+	internal class ProductMetadata
+	{
+		[Required(ErrorMessage ="商品名稱未填寫!")]
+		[Display(Name ="商品名稱")]
+		[StringLength(maximumLength:40,MinimumLength =3,ErrorMessage ="商品名稱至少3個字")]
+		public string ProductName { get; set; } = null!;
+
+		[DisplayFormat(DataFormatString ="{0:C}")]
+		[Display(Name ="商品單價")]
+		public decimal? UnitPrice { get; set; }
+
+		[Display(Name ="訂購數量")]
+		[Range(1,100,ErrorMessage ="{0}必須介於{1}~{2}之間")]
+		public short? UnitsOnOrder { get; set; }
+	}
+}
---
+++ CategoryProducts/Partial/Product.cs
@@ -0,0 +1,11 @@
+﻿using CategoryProducts.Metadata;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoryProducts.Models
+{
+	[ModelMetadataType(typeof(ProductMetadata))]
+	public partial class Product
+	{
+		
+	}
+}
